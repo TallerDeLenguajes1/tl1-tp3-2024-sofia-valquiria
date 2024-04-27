73c7ca376a156d163260d525207eed9b7d5702cd
Sofia Sandoval <sandovalsofi99@gmail.com>
2024-04-27 10:35:39 -0300

    2) completado

--- TP3-2.c
+++ TP3-2.c
@@ -12,16 +12,38 @@ cuándo ocurrió.*/
 void main(){
     int produccion[5][12];
     int promedioAnio = 0;
+    int valorMaximo = 0;
+    int valorMinimo = 50000;
+    int mesMaximo = 0;
+    int anioMaximo = 0;
+    int mesMinimo = 0;
+    int anioMinimo = 0;
     srand(time(NULL));
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 12; j++)
         {
             produccion[i][j] = rand()%40000+10000;
+            if(produccion[i][j]>= valorMaximo){
+                valorMaximo = produccion[i][j];
+        		anioMaximo = i+1;
+                mesMaximo = j;
+            }
+            if(produccion[i][j]<= valorMinimo)
+            {
+                valorMinimo = produccion[i][j];
+                anioMinimo = i+1;
+                mesMinimo = j;
+            }
+            
             printf("%d ", produccion[i][j]);
             promedioAnio += produccion[i][j];
         }
         promedioAnio = promedioAnio / 12;
         printf("El promedio del %d° año es: %d \n",i+1,promedioAnio);
-    }   
+        
+    }
+    printf("La mayor produccion fue de %d, el mes %d, del anio %d\n",produccion[anioMaximo][mesMaximo], mesMaximo, anioMaximo);
+    printf("La menor produccion fue de %d, el mes %d, del anio %d\n",produccion[anioMinimo][mesMinimo], mesMaximo, anioMinimo);         
+    
 }
